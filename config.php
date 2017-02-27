c4e5adc263747e660c9c4798c9c1c96b062c0b4f
ariejoosse <ariejoosse@users.noreply.github.com>
2017-02-27 16:09:25 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>ITSM integration</title>
+        <link href="/css/nxt_includes.css" rel="stylesheet" type="text/css">
+        <link href="/css/includes.css" rel="stylesheet" type="text/css">
+    </head>
+<body class="setup_body" >
+<img src="/images/logo.svg" style="padding-bottom:10px">
+
+<div class="wrap">
+
+<form id="thisform">
+<div class="left_col"><div class="caption cell"></div>We need a device to determine the 'normal' values for the metrics:</div><div class="left_col">Device name: <input type="text"></div><div class="clr"></div>  
+<div class="left_col"><div class="caption cell"></div>Edit exisiting metrics.</div><div class="left_col"><input type="submit" name="operation" value="Edit metrics"></div><div class="clr"></div>    
+<div class="left_col"><div class="caption cell"></div>Create metrics from scratch or edit metrics after changing the Nexthink query</div><div class="left_col"><input type="submit" name="operation" value="New metrics"></div>  <div class="clr"></div>  
+</form>
+</div>
+<p style="clear:both; padding-bottom:15px;">
+
